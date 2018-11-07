76dea2a7db823553579d69ede06045072c2ecefb
lissylasagne <lissy_lissy1@yahoo.de>
2018-11-07 16:58:50 +0100

    things happend with stars

--- application/source/application_solar.cpp
+++ application/source/application_solar.cpp
@@ -51,9 +51,8 @@ void ApplicationSolar::render() const {
   renderPlanet(sun);
   renderPlanet(planet1);
   renderPlanet(planet2);
-  //renderPlanet(&planet3);
   renderPlanet(moon);
-  //renderStars();
+  renderStars();
 }
 
 
@@ -64,11 +63,17 @@ void ApplicationSolar::uploadView() {
   // upload matrix to gpu
   glUniformMatrix4fv(m_shaders.at("planet").u_locs.at("ViewMatrix"),
                      1, GL_FALSE, glm::value_ptr(view_matrix));
+
+  glUniformMatrix4fv(m_shaders.at("stars").u_locs.at("ViewMatrix"),
+                     1, GL_FALSE, glm::value_ptr(view_matrix));
+
 }
 
 void ApplicationSolar::uploadProjection() {
   // upload matrix to gpu
   glUniformMatrix4fv(m_shaders.at("planet").u_locs.at("ProjectionMatrix"),
+                     1, GL_FALSE, glm::value_ptr(m_view_projection));
+  glUniformMatrix4fv(m_shaders.at("stars").u_locs.at("ProjectionMatrix"),
                      1, GL_FALSE, glm::value_ptr(m_view_projection));
 }
 
@@ -76,6 +81,7 @@ void ApplicationSolar::uploadProjection() {
 void ApplicationSolar::uploadUniforms() { 
   // bind shader to which to upload unforms
   glUseProgram(m_shaders.at("planet").handle);
+  glUseProgram(m_shaders.at("stars").handle);
   // upload uniform values to new locations
   uploadView();
   uploadProjection();
@@ -92,6 +98,15 @@ void ApplicationSolar::initializeShaderPrograms() {
   m_shaders.at("planet").u_locs["ModelMatrix"] = -1;
   m_shaders.at("planet").u_locs["ViewMatrix"] = -1;
   m_shaders.at("planet").u_locs["ProjectionMatrix"] = -1;
+
+  // store shader program objects in container
+  m_shaders.emplace("stars", shader_program{{{GL_VERTEX_SHADER,m_resource_path + "shaders/stars.vert"},
+                                           {GL_FRAGMENT_SHADER, m_resource_path + "shaders/stars.frag"}}});
+  // request uniform locations for shader program
+  m_shaders.at("stars").u_locs["NormalMatrix"] = -1;
+  m_shaders.at("stars").u_locs["ModelMatrix"] = -1;
+  m_shaders.at("stars").u_locs["ViewMatrix"] = -1;
+  m_shaders.at("stars").u_locs["ProjectionMatrix"] = -1;
 }
 
 // load models
@@ -266,10 +281,19 @@ void ApplicationSolar::renderStars() const {
   glBindVertexArray(star_object.vertex_AO);
   glPointSize(1.0);
   glDrawArrays(star_object.draw_mode, 0, (int)m_stars.size());
+  //glDrawElements(star_object.draw_mode, (int)m_stars.size(), model::INDEX.type, NULL);
+
 }
 
 
 void ApplicationSolar::initializeStars(int numberStars) {
+  glm::fmat4 unitmat{ 1.0f, 0.0f, 0.0f, 0.0f, 
+                      0.0f, 1.0f, 0.0f, 0.0f,
+                      0.0f, 0.0f, 1.0f, 0.0f, 
+                      0.0f, 0.0f, 0.0f, 1.0f};
+  GeometryNode* starNode = new GeometryNode("stars", unitmat, unitmat, m_star_model);
+  m_scene.getRoot()->addChildren(starNode);
+
   std::vector<float> stars(numberStars*6);
 
   //6 floats for each star: position and colour
@@ -284,6 +308,8 @@ void ApplicationSolar::initializeStars(int numberStars) {
     stars[i] = random;
   }
   m_stars = stars;
+
+    std::cout << m_stars.size() << ' ';
 }
 
 
